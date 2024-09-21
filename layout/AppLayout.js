590154ead6615ad26dc51411a2bf90723190132c
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2024-09-21 22:23:42 +0200

    setup: installed mobx

--- layout/AppLayout.js
+++ layout/AppLayout.js
@@ -5,12 +5,10 @@ const AppLayout = ({ children }) => {
   return (
     <main className="relative min-h-screen bg-background">
       <Navbar />
-      <Container>
-        <Container as="section" className="px-20 pb-5 pt-20">
-          {children}
-        </Container>
-        <Container className="absolute top-0 h-screen w-screen bg-backDrop opacity-30" />
+      <Container as="section" className="px-20 pb-5 pt-20">
+        {children}
       </Container>
+      <Container className="absolute top-0 h-screen w-screen bg-backDrop opacity-30" />
       <WidgetsLayout />
     </main>
   );
