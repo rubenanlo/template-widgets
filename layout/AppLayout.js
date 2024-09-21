d0df92367fb12021acb8e52bddd7ad72e439eb1e
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2024-09-21 21:13:26 +0200

    chorus: initial appLayout

--- layout/AppLayout.js
+++ layout/AppLayout.js
@@ -1,9 +1,12 @@
-import Navbar from "@/components/Navbar";
+import { Container } from "@/components/ui/Container";
 const AppLayout = ({ children }) => {
   return (
-    <main className="bg-background font-interphase">
-      <Navbar />
-      {children}
+    <main className="relative min-h-screen bg-background">
+      <Container>
+        <Container as="section" className="px-20 pb-5 pt-20">
+          {children}
+        </Container>
+      </Container>
     </main>
   );
 };
