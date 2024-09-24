5b8cf17e882c1ced23bb7c8f6951c5b14678652a
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2024-09-24 22:15:45 +0200

    refactor: better readability for WidgetsLayout

--- layout/WidgetsLayout.js
+++ layout/WidgetsLayout.js
@@ -1,9 +1,9 @@
 import { observer } from "mobx-react-lite";
+import WidgetNews from "@/components/widgets/WidgetNews";
 import { Show } from "@/components/ui/Show";
 import { Container } from "@/components/ui/Container";
 import { showUp, toggleWidget } from "@/library/animations";
 import { useGeneralStore } from "@/providers/generalStore";
-import WidgetNews from "@/components/widgets/WidgetNews";
 
 const BackDrop = (props) => (
   <Container.Animated
@@ -13,31 +13,27 @@ const BackDrop = (props) => (
   />
 );
 
-const WidgetsToggle = ({ isWidgetsOn, setIsWidgetsOn }) => (
-  <Container.Animated
-    onMouseEnter={() => setIsWidgetsOn(!isWidgetsOn)}
-    onMouseLeave={() => setIsWidgetsOn(!isWidgetsOn)}
-    {...toggleWidget(isWidgetsOn)}
-    // component={isWidgetsOn && <WidgetsLayout />}
-  />
-);
 const WidgetsLayout = () => {
   const { isWidgetsOn, setIsWidgetsOn } = useGeneralStore();
 
   return (
     <>
-      <WidgetsToggle
-        isWidgetsOn={isWidgetsOn}
-        setIsWidgetsOn={setIsWidgetsOn}
-      />
       <Show isTrue={isWidgetsOn} animatePresence>
         <BackDrop />
       </Show>
-      <Container className="fixed inset-y-0 right-0 flex h-full w-96 overflow-auto bg-white py-16 scrollbar-hide">
-        <Container className="mx-auto flex w-4/5 flex-col space-y-6 border">
-          <WidgetNews />
-        </Container>
-      </Container>
+      <Container.Animated
+        onMouseEnter={() => setIsWidgetsOn(!isWidgetsOn)}
+        onMouseLeave={() => setIsWidgetsOn(!isWidgetsOn)}
+        {...toggleWidget(isWidgetsOn)}
+      >
+        <Show isTrue={isWidgetsOn}>
+          <Container className="fixed inset-y-0 right-0 flex h-full w-96 overflow-auto py-16 scrollbar-hide">
+            <Container className="mx-auto flex w-4/5 flex-col space-y-6">
+              <WidgetNews />
+            </Container>
+          </Container>
+        </Show>
+      </Container.Animated>
     </>
   );
 };
