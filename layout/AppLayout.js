d9a6fa0dd31c06beceffbd30b342e198a7680bdf
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2024-09-24 16:03:53 +0200

    chorus: added animation to toggle widgets container

--- layout/AppLayout.js
+++ layout/AppLayout.js
@@ -4,7 +4,7 @@ import Navbar from "@/components/Navbar";
 import { Container } from "@/components/ui/Container";
 import { Show } from "@/components/ui/Show";
 import { useGeneralStore } from "@/providers/generalStore";
-import { showUp } from "@/library/animations";
+import { showUp, toggleWidget } from "@/library/animations";
 // import WidgetsLayout from "./WidgetsLayout";
 
 const BackDrop = (props) => (
@@ -15,47 +15,25 @@ const BackDrop = (props) => (
   />
 );
 
-const WidgetsToggle = ({ isWidgetsOn, setIsWidgetsOn }) => {
-  const animate = (isWidgetsOn) => {
-    const hidden = {
-      position: "absolute",
-      right: 0,
-      height: "2rem", // 8 in tailwind
-      width: "1.25rem", // 5 in tailwind
-      marginTop: "2.5rem", // 10 in tailwind
-      borderRadius: "1rem", // rounded-2xl
-      borderBottomLeftRadius: 0, // rounded-br-none
-      borderBottomRightRadius: 0, // rounded-tr-none
-      borderTop: "1px solid rgba(255, 255, 255, 0.5)", // border-y border-white/50
-      borderLeft: "1px solid rgba(255, 255, 255, 0.5)", // border-l
-      zIndex: 10,
-    };
-    const visible = {
-      position: "relative",
-    };
-    return isWidgetsOn ? visible : hidden;
-  };
-  return (
-    <Container.Animated
-      onMouseEnter={() => setIsWidgetsOn(!isWidgetsOn)}
-      onMouseLeave={() => setIsWidgetsOn(!isWidgetsOn)}
-      animate={animate(isWidgetsOn)}
-    />
-  );
-};
+const WidgetsToggle = ({ isWidgetsOn, setIsWidgetsOn }) => (
+  <Container.Animated
+    onMouseEnter={() => setIsWidgetsOn(!isWidgetsOn)}
+    onMouseLeave={() => setIsWidgetsOn(!isWidgetsOn)}
+    {...toggleWidget(isWidgetsOn)}
+    // component={isWidgetsOn && <WidgetsLayout />}
+  />
+);
 
 const AppLayout = ({ children }) => {
   const { isWidgetsOn, setIsWidgetsOn } = useGeneralStore();
 
   return (
     <main className="relative h-screen bg-background">
+      <WidgetsToggle
+        isWidgetsOn={isWidgetsOn}
+        setIsWidgetsOn={setIsWidgetsOn}
+      />
       <Navbar />
-      <Show animatePresence>
-        <WidgetsToggle
-          isWidgetsOn={isWidgetsOn}
-          setIsWidgetsOn={setIsWidgetsOn}
-        />
-      </Show>
       <Show isTrue={isWidgetsOn} animatePresence>
         <BackDrop />
       </Show>
--- library/animations.js
+++ library/animations.js
@@ -1,8 +1,47 @@
+import tailwindConfig from "@/tailwind.config.js";
+import resolveConfig from "tailwindcss/resolveConfig";
+
+const {
+  theme: { colors },
+} = resolveConfig(tailwindConfig);
+
 // Goes with animatePresence hook
 export const showUp = {
   initial: "hidden",
   animate: "visible",
   exit: "hidden",
-  variants: { hidden: { opacity: 0 }, visible: { opacity: 0.7 } },
+  variants: { hidden: { opacity: 0 }, visible: { opacity: 0.8 } },
   transition: { duration: 0.5 },
+};
+
+// Goes with WidgetsToggle component
+export const toggleWidget = (isWidgetsOn) => {
+  const hidden = {
+    position: "absolute",
+    right: "20px",
+    marginTop: "1.2rem",
+    width: "2rem",
+    height: "2rem",
+    borderLeft: "1rem solid transparent",
+    borderRight: "1rem solid transparent",
+    borderTop: `1.5rem solid ${colors.foreground}`,
+    zIndex: 10,
+  };
+  const visible = {
+    position: "fixed",
+    insetY: 0,
+    right: 0,
+    marginTop: "0rem",
+    marginRight: "0rem",
+    height: "100%",
+    width: "24rem",
+    overflow: "auto",
+    backgroundColor: [colors.foreground, colors.background],
+    paddingTop: "4rem",
+    zIndex: 10,
+    transition: {
+      duration: 0.3,
+    },
+  };
+  return isWidgetsOn ? { animate: visible } : { animate: hidden };
 };
