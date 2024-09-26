f2cf605a8fa13884f481c5b85330132389d82b56
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2024-09-26 20:47:26 +0200

    chorus: added pattern as background of applayout

--- layout/AppLayout.js
+++ layout/AppLayout.js
@@ -3,17 +3,15 @@ import Cloud from "@/components/patterns/Cloud";
 import { Container } from "@/components/ui/Container";
 import WidgetsLayout from "@/layout/WidgetsLayout";
 
-const AppLayout = ({ children }) => {
-  return (
-    <main className="relative h-screen bg-background">
-      <WidgetsLayout />
-      <Navbar />
-      <Container as="section" className="px-20 pb-5 pt-20">
-        <Cloud />
-        {children}
-      </Container>
-    </main>
-  );
-};
+const AppLayout = ({ children }) => (
+  <main className="relative h-screen bg-background">
+    <WidgetsLayout />
+    <Navbar />
+    <Cloud />
+    <Container as="section" className="px-20 pb-5 pt-20">
+      {children}
+    </Container>
+  </main>
+);
 
 export default AppLayout;
