abc942b99589c006659e8bbdf40815376af08bda
Ruben Andino <85644491+rubenanlo@users.noreply.github.com>
2024-09-27 21:30:16 +0200

    to-revert: added a mock widget

---
+++ components/widgets/WidgetMock.js
@@ -0,0 +1,5 @@
+const WidgetMock = () => (
+  <div>This is a mock widget, the real one is the news widget</div>
+);
+
+export default WidgetMock;
--- library/widgets.js
+++ library/widgets.js
@@ -1,8 +1,15 @@
+import WidgetMock from "@/components/widgets/WidgetMock";
 import WidgetNews from "@/components/widgets/WidgetNews";
 
 export const widgets = [
   {
     name: "Your news",
     component: <WidgetNews />,
+    display: false,
+  },
+  {
+    name: "Your calendar",
+    component: <WidgetMock />,
+    display: false,
   },
 ];
